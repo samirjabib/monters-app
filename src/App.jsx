1649900d5d202c298a4b2f350108c8037e0b0fbb
samirjabib <samir.jc123@gmail.com>
2022-08-15 22:53:48 -0500

    add explain react renders and re renders

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,37 @@ class App extends Component{ //Componente Funcional
     super();
 
     this.state ={
-      name:'Yihua'
+      monsters:[]
     }
+    console.log('1')
+  }
+
+  componentDidMount(){
+    console.log('3')
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState(
+        () => {
+        return {monsters: users}
+        },
+        () => {
+          console.log(this.state.monsters)
+        }
+        
+      ))
+      
   }
 
   render(){
+    console.log('2')
     return(
-      <div className='App'>
-        <h1>Hello {this.state.name}</h1>
-        <button oncClick = { () => {
-          this.setState({name:'samir'})
-        }}>Change Name</button>
 
+      <div className='App'>
+        {
+          this.state.monsters.map( (monster) => {
+            return <h1 key={monster.name}>{monster.name}</h1>
+          })
+        }
       </div>
     )
   }
